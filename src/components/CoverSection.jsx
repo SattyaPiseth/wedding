edd596bdc3d8af0a9dbce9eb3185aaecde8685e7
Sattya Piseth <sattya.piseth@technovage.io>
2025-09-29 21:31:17 +0700

    Auto update: 2025-09-29 21:31:16

--- src/components/CoverSection.jsx
+++ src/components/CoverSection.jsx
@@ -4,20 +4,20 @@ export default function CoverSection({ isStoryPlaying = false, onStart }) {
       className="
         flex flex-col items-center justify-center
         text-[var(--gold)] tracking-wider
-        gap-3 
+      
       "
     >
       {!isStoryPlaying && (
         <>
           <img
-            className="block mx-auto h-auto w-[clamp(56%,70vw,82%)] max-w-[min(350px,70%)]"
+            className="block mx-auto h-auto w-[clamp(56%,70vw,82%)] max-w-[min(300px,60%)]"
             src="/images/cover-page/name-cover.png"
             alt="Cover"
           />
           <span
             className="
               moul-regular text-center
-              text-[clamp(1.15rem,4vw,1.75rem)]
+              text-[clamp(1.15rem,3.5vw,1.75rem)]
               animate-[fade-up_700ms_ease-out_both] [animation-delay:120ms]
             "
           >
@@ -26,8 +26,8 @@ export default function CoverSection({ isStoryPlaying = false, onStart }) {
 
           <span
             className="
-              moul-regular text-center
-              text-[clamp(1.05rem,3.2vw,1.5rem)] py-5
+              moul-regular text-center 
+              text-[clamp(1.05rem,5vw,1.5rem)] py-5
               animate-[fade-up_700ms_ease-out_both] [animation-delay:240ms]
             "
           >
@@ -37,12 +37,12 @@ export default function CoverSection({ isStoryPlaying = false, onStart }) {
           <button
             onClick={onStart}
             className="
-              px-6 py-2 ios:px-7 ios:py-3 rounded-xl font-semibold moul-regular
-              text-[var(--gold)] bg-white/5 backdrop-blur-sm text-sm
+              px-6 py-2 rounded-xl font-semibold moul-regular
+              text-[var(--gold)] bg-white/5 backdrop-blur-sm
               border border-[var(--gold)]/60 shadow-md shadow-black/20
-              hover:bg-white/10 transition duration-300
+              hover:bg-white/10 transition duration-300 text-lg
               focus:outline-none focus:ring-2 focus:ring-[var(--gold)]/40
-              animate-[fade-up_700ms_ease-out_both]
+              animate-[fade-up_700ms_ease-out_both] 
             "
           >
             ចូលរួមកម្មវិធី
