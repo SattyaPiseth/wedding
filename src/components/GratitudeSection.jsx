2df35ecbcfc86ea31df103413f8a2939bda6a235
Sattya Piseth <sattya.piseth@technovage.io>
2025-10-03 13:58:46 +0700

    Auto update: 2025-10-03 13:58:46

--- src/components/GratitudeSection.jsx
+++ src/components/GratitudeSection.jsx
@@ -6,24 +6,26 @@ export const GratitudeSection = () => {
         <section
           aria-labelledby="gratitude-title"
           className="my-[5vh] mx-auto max-w-screen-md"
-          data-aos="fade-up"
-     data-aos-anchor-placement="bottom-bottom"
         >
           <h3
             id="gratitude-title"
             className="moul-regular text-base sm:text-lg tracking-wide text-center text-balance"
+            data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom"
           >
             សេចក្ដីថ្លែងអំណរគុណ និង សូមអភ័យទោស
           </h3>
 
-          <p className="siemreap-regular mt-4 text-sm sm:text-base md:text-lg leading-relaxed md:leading-8 tracking-wide">
+          <p className="siemreap-regular mt-4 text-sm sm:text-base md:text-lg leading-relaxed md:leading-8 tracking-wide" data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom">
             យើងខ្ញុំជាមាតាបិតា របស់កូនទាំងពីរ សូមថ្លែងអំណរគុណយ៉ាងជ្រាលជ្រៅ ចំពោះវត្តមានដ៏ឧត្តុង្គឧត្តមរបស់
             សម្ដេច ឯកឧត្តម លោកជំទាវ លោកអ្នកឧកញ៉ា ឧកញ៉ា លោក លោកស្រី អ្នកនាង កញ្ញា និងប្រិយមិត្ត
             ដែលបានអញ្ជើញចូលរួមជាកិត្តិយស ក្នុងពិធីសិរីសួស្តីអាពាហ៍ពិពាហ៍ កូនប្រុស-កូនស្រី របស់យើងខ្ញុំ
             នាពេលខាងមុខនេះ។
           </p>
 
-          <p className="siemreap-regular mt-[3vh] text-sm sm:text-base md:text-lg leading-relaxed md:leading-8 tracking-wide">
+          <p className="siemreap-regular mt-[3vh] text-sm sm:text-base md:text-lg leading-relaxed md:leading-8 tracking-wide" data-aos="fade-up"
+            data-aos-anchor-placement="bottom-bottom">
             យើងខ្ញុំសូមខន្តីអភ័យទោស ដែលមិនអាចជូនលិខិតអញ្ជើញដោយផ្ទាល់បានដោយការគោរព សូមទទួលគារវកិច្ចដ៏ខ្ពង់ខ្ពស់
             ពីយើងខ្ញុំ៕
           </p>
